b5446d2679dc70db77185a7a6e89da4515b5a7ae
michal <zenczak.michal@gmail.com>
2018-04-03 15:12:12 +0200

    updated LibraryItemActivity

--- app/src/main/java/com/example/michal/musicapp/LibraryItemActivity.java
+++ app/src/main/java/com/example/michal/musicapp/LibraryItemActivity.java
@@ -23,36 +23,28 @@ public class LibraryItemActivity extends AppCompatActivity {
         TextView selectedItemName = findViewById(R.id.selected_item_id);
         selectedItemName.setText(selectedItem);
 
+        ArrayList<Item> selectedItemList = new ArrayList<>();
         if (getResources().getString(selectedItem).equals(getResources().getString(R.string.library_albums))){
-            ArrayList<Item> selectedItemList = new ArrayList<>();
             for (int i=1; i<=20; i++){
                 selectedItemList.add(new Item(makeSingular(getResources().getString(selectedItem)) + " " + i,
                         makeSingular(getResources().getString(R.string.library_artists)) + " " + i,
                         R.drawable.ic_album_black_24dp));
             }
-            ListView listView = findViewById(R.id.library_selected_item_list);
-            ItemAdapter adapter = new ItemAdapter(getApplicationContext(), selectedItemList);
-            listView.setAdapter(adapter);
         }else if (getResources().getString(selectedItem).equals(getResources().getString(R.string.library_artists))){
-            ArrayList<Item> selectedItemList = new ArrayList<>();
+
             for (int i=1; i<=20; i++){
                 selectedItemList.add(new Item(makeSingular(getResources().getString(selectedItem)) + " " + i,
                         R.drawable.ic_people_black_24dp));
             }
-            ListView listView = findViewById(R.id.library_selected_item_list);
-            ItemAdapter adapter = new ItemAdapter(getApplicationContext(), selectedItemList);
-            listView.setAdapter(adapter);
         }else if (getResources().getString(selectedItem).equals(getResources().getString(R.string.library_songs))){
-            ArrayList<Item> selectedItemList = new ArrayList<>();
             for (int i=1; i<=20; i++){
                 selectedItemList.add(new Item(getResources().getString(R.string.song_title) + " " + i,
                         makeSingular(getResources().getString(R.string.library_artists)) + " " + i));
             }
-            ListView listView = findViewById(R.id.library_selected_item_list);
-            ItemAdapter adapter = new ItemAdapter(getApplicationContext(), selectedItemList);
-            listView.setAdapter(adapter);
         }
-
+        ListView listView = findViewById(R.id.library_selected_item_list);
+        ItemAdapter adapter = new ItemAdapter(getApplicationContext(), selectedItemList);
+        listView.setAdapter(adapter);
 
     }
     /**
