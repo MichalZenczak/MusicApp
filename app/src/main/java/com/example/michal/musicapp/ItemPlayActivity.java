296f3f9e07b0b0af07a40590d911275b25269fd4
michal <zenczak.michal@gmail.com>
2018-04-03 17:02:16 +0200

    added onClickListeners to buttons

--- app/src/main/java/com/example/michal/musicapp/ItemPlayActivity.java
+++ app/src/main/java/com/example/michal/musicapp/ItemPlayActivity.java
@@ -14,18 +14,45 @@ public class ItemPlayActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_item_play);
 
-        final Button playButton = findViewById(R.id.button_play);
+        Button playButton = findViewById(R.id.button_play);
         playButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast playToast = Toast.makeText(ItemPlayActivity.this, "A song is playing!", Toast.LENGTH_SHORT );
+                Toast playToast = Toast.makeText(ItemPlayActivity.this, getResources().getString(R.string.play_toast), Toast.LENGTH_SHORT );
                 playToast.setGravity(Gravity.CENTER,0,0);
                 playToast.show();
 
             }
         });
 
+        Button stopButton = findViewById(R.id.button_stop);
+        stopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast stopToast = Toast.makeText(ItemPlayActivity.this, getResources().getString(R.string.stop_toast),Toast.LENGTH_SHORT );
+                stopToast.setGravity(Gravity.CENTER,0,0);
+                stopToast.show();
+            }
+        });
 
+        Button nextButton = findViewById(R.id.button_next);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast nextToast = Toast.makeText(ItemPlayActivity.this, getResources().getString(R.string.next_toast),Toast.LENGTH_SHORT);
+                nextToast.setGravity(Gravity.CENTER,0,0);
+                nextToast.show();
+            }
+        });
 
+        Button previousButton = findViewById(R.id.button_previous);
+        previousButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast previousToast = Toast.makeText(ItemPlayActivity.this, getResources().getString(R.string.previous_toast), Toast.LENGTH_SHORT);
+                previousToast.setGravity(Gravity.CENTER,0,0);
+                previousToast.show();
+            }
+        });
     }
 }
