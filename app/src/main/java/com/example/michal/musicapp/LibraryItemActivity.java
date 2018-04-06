c5d2d72b5678eee3f1f52e778fe1f7d6a41d551e
Michal <zenczak.michal@gmail.com>
2018-04-06 20:45:41 +0200

    trying to solve an error!!!

--- app/src/main/java/com/example/michal/musicapp/LibraryItemActivity.java
+++ app/src/main/java/com/example/michal/musicapp/LibraryItemActivity.java
@@ -2,6 +2,7 @@ package com.example.michal.musicapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -13,31 +14,48 @@ import android.widget.TextView;
 import java.util.ArrayList;
 
 public class LibraryItemActivity extends AppCompatActivity {
-    final private int LIST_ITEM_COUNT = 20;
+
+    //This part of code is new
+    private int mSelectedItem;
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putInt("selectedItem", mSelectedItem);
+    }
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        final int LIST_ITEM_COUNT = 20;
+
+        //This if statement is new
         super.onCreate(savedInstanceState);
+        if (savedInstanceState != null){
+            mSelectedItem = savedInstanceState.getInt("selectedItem");
+        }else {
+            Intent myIntent = getIntent();
+            mSelectedItem = getSelectedItem(myIntent);
+        }
+
         setContentView(R.layout.activity_item_library);
 
-        Intent myIntent = getIntent();
-        final int selectedItem = myIntent.getIntExtra("selectedItem",0);
-
         TextView selectedItemName = findViewById(R.id.selected_item_id);
-        selectedItemName.setText(selectedItem);
+
+        //This line of code is new
+        selectedItemName.setText(String.valueOf(getResources().getString(mSelectedItem)));
 
         final ArrayList<Item> selectedItemList = new ArrayList<>();
-        if (getResources().getString(selectedItem).equals(getResources().getString(R.string.library_albums))){
+        if (getResources().getString(mSelectedItem).equals(getResources().getString(R.string.library_albums))){
             for (int i=1; i<=LIST_ITEM_COUNT; i++){
-                selectedItemList.add(new Item(makeSingular(getResources().getString(selectedItem)) + " " + i,
+                selectedItemList.add(new Item(makeSingular(getResources().getString(mSelectedItem)) + " " + i,
                         makeSingular(getResources().getString(R.string.library_artists)) + " " + i, R.drawable.ic_album_black_24dp));
             }
-        }else if (getResources().getString(selectedItem).equals(getResources().getString(R.string.library_artists))){
+        }else if (getResources().getString(mSelectedItem).equals(getResources().getString(R.string.library_artists))){
             for (int i=1; i<=LIST_ITEM_COUNT; i++){
-                selectedItemList.add(new Item(makeSingular(getResources().getString(selectedItem)) + " " + i,
+                selectedItemList.add(new Item(makeSingular(getResources().getString(mSelectedItem)) + " " + i,
                         R.drawable.ic_people_black_24dp));
             }
-        }else if (getResources().getString(selectedItem).equals(getResources().getString(R.string.library_songs))){
+        }else if (getResources().getString(mSelectedItem).equals(getResources().getString(R.string.library_songs))){
             for (int i=1; i<=LIST_ITEM_COUNT; i++){
                 selectedItemList.add(new Item(getResources().getString(R.string.song_title) + " " + i, makeSingular(getResources().getString(R.string.library_artists)) + " " + i));
             }
@@ -68,5 +86,10 @@ public class LibraryItemActivity extends AppCompatActivity {
      */
     private String makeSingular(String plural){
         return plural.substring(0, plural.length() - 1);
+    }
+
+    // This method is new
+    private int getSelectedItem(Intent myIntent){
+        return myIntent.getIntExtra("selectedItem",0);
     }
 }
